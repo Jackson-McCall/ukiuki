3f8e9b61328fabb4cf13d19e9f3eda38c100ee8d
Jack McCall <jackson.mccall907@gmail.com>
2026-02-11 18:23:22 -0600

    Added CORS headers so that the javascript can read the response from the API

--- Controllers/PingController.cpp
+++ Controllers/PingController.cpp
@@ -16,6 +16,10 @@ void PingController::healthCheck(const drogon::HttpRequestPtr& req,
 	// Set the body content
 	resp->setBody("UkiUkiAPI health status = UP");
 
+	// CORS (Cross-Origin Resource Sharing) blocks requests from different origins than the API
+	// Add CORS headers, since this is health, anyone can check
+	resp->addHeader("Access-Control-Allow-Origin", "*");
+
 	// Invoke the callback. This sends the response through the socket 
 	callback(resp);
 }
